0bef148d3c332bec7a9499d9c8bf6d806d8fd5f2
Bongani Baloyi <bonganibaloyi94@gmail.com>
2023-05-11 18:17:40 +0000

    API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,53 +1,54 @@
 #!/usr/bin/python3
-""" 0. Count it!
-"""
+""" raddit api"""
+
+import json
 import requests
 
 
-def hot_dict_fill(response, word_list, hot_dict, after):
-    """ Fill hot dict increment count words
-    """
-    titles = response.json().get("data").get("children")
-    for title in titles:
-        for hot_word in word_list:
-            title_post = title.get("data").get("title")
-            if title_post:
-                words_in_title = title_post.split()
-                for word_title in words_in_title:
-                    if hot_word.lower() == word_title.lower():
-                        hot_dict[hot_word] += 1
-    if not after:
-        for k, v in sorted(hot_dict.items(),
-                           key=lambda items: (items[1], items[0]),
-                           reverse=True):
-            if v != 0:
-                print("{}: {}".format(k, v))
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
 
+    if after == "":
+        count = [0] * len(word_list)
 
-def count_words(subreddit, word_list, after=None, hot_dict={}):
-    """Write a recursive function that queries the Reddit API, parses the
-    title of all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as
-    javascript, but java should not).
-    """
-    headers = {'User-Agent': 'DiegoOrejuela'}
-    params = {"limit": 100, 'after': after}
-    response = requests.get("https://www.reddit.com/r/{}/hot/.json".
-                            format(subreddit), headers=headers, params=params)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
 
-    if len(hot_dict) == 0:
-        for hot_word in word_list:
-            hot_dict[hot_word] = 0
+    if request.status_code == 200:
+        data = request.json()
 
-    if response.status_code == 200:
-        after_response = response.json().get("data").get("after")
-        if after_response:
-            count_words(subreddit, word_list,
-                        after=after_response, hot_dict=hot_dict)
-            hot_dict_fill(response, word_list, hot_dict, after)
-            return(hot_dict)
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
-            hot_dict_fill(response, word_list, hot_dict, after)
-            return(hot_dict)
-    else:
-        return(None)
+            count_words(subreddit, word_list, after, count)
